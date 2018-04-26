5ef2891010fb0db36a848f3968eb4aa59b82de9c
elzambon <elzambon@uc.cl>
2018-04-26 12:04:52 -0300

    Esta todo inicializado

--- T2/T02_mapa.py
+++ T2/T02_mapa.py
@@ -1,11 +1,11 @@
 from abc import ABCMeta
-from T02_limpieza import es_salto_linea, cambiar_salto
-
+from T02_limpieza import limpieza
 
 class Celda(metaclass=ABCMeta):
-    def __init__(self, pos_x, pos_y):
+    def __init__(self, pos_x, pos_y, coordenada):
         self.pos_x = pos_x
         self.pos_y = pos_y
+        self.coordenada = coordenada
         self.nodo_izq = None
         self.nodo_der = None
         self.nodo_arriba = None
@@ -17,37 +17,59 @@ class Celda(metaclass=ABCMeta):
 
 
 class Obstaculo(Celda):
-    def __init__(self, pos_x, pos_y):
-        super(Obstaculo, self).__init__(pos_x, pos_y)
+    def __init__(self, pos_x, pos_y, coordenada):
+        super(Obstaculo, self).__init__(pos_x, pos_y, coordenada)
 
 
 class Espacio(Celda):
-    def __init__(self, pos_x, pos_y, dificultad):
-        super(Espacio, self).__init__(pos_x, pos_y)
+    def __init__(self, pos_x, pos_y, dificultad, coordenada):
+        super(Espacio, self).__init__(pos_x, pos_y, coordenada)
         self.dificultad = dificultad
 
 
 class Grafo:
     def __init__(self):
-        file = open('mapa_arreglado.txt', "r", encoding='utf-8')
-        self.mapa = [linea.split(',') for linea in file]
-        self.mapa = [cambiar_salto(linea) for linea in self.mapa]
+        self.mapa = []
 
     def agregar_nodos(self):
-        for linea in range(len(self.mapa)):
-            for vertice in range(len(self.mapa[linea])):
-                if self.mapa[linea][vertice] == "X":
-                    self.mapa[linea][vertice] = Obstaculo(vertice, linea)
-                else:
-                    self.mapa[linea][vertice] = Espacio(vertice, linea, self.mapa[linea][vertice])
+        limpieza()
+        datos = []
+        largos = []
+        lectura = open('mapa_arreglado.txt', "r", encoding='utf-8')
+        for linea in lectura:
+            linea = linea.replace('\n', "")
+            lista = linea.split(',')
+            largos.append(len(lista))
+            datos.append(lista)
+        columnas = max(largos)
+        filas = len(datos)
+
+        for y in range(filas):
+            self.mapa.append([])
+            for x in range(columnas):
+                coordenada = str(chr(y+65)) + str(x+1)
+                try:
+                    dato = datos[y][x]
+                    if dato == 'X':
+                        obstaculo = Obstaculo(x,y, coordenada)
+                        self.mapa[y].append(obstaculo)
+                    else:
+                        camino = Espacio(x,y, dato, coordenada)
+                        self.mapa[y].append(camino)
+                except:
+                    vacio = Espacio(x,y,0,coordenada)
+                    self.mapa[y].append(vacio)
 
     def actualizar_nodos(self):
         for linea in self.mapa:
             for nodo in linea:
                 x = nodo.pos_x
                 y = nodo.pos_y
-                if self.mapa[y][x-1] and x > 0:
-                    nodo.nodo_izq = self.mapa[y][x-1]
+                try:
+                    if x > 0:
+                        nodo.nodo_izq = self.mapa[y][x-1]
+                except IndexError:
+                    pass
                 try:
                     nodo.nodo_der = self.mapa[y][x+1]
                 except IndexError:
@@ -56,23 +78,34 @@ class Grafo:
                     nodo.nodo_abajo = self.mapa[y+1][x]
                 except IndexError:
                     pass
-                if y > 0:
-                    try:
+                try:
+                    if y > 0:
                         nodo.nodo_arriba = self.mapa[y - 1][x]
-                    except IndexError:
+                except IndexError:
+                    pass
+        for linea in self.mapa:
+            for nodo in linea:
+                x = nodo.pos_x
+                y = nodo.pos_y
+                try:
+                    if x > 0 and y > 0:
+                        nodo.nodo_diag_izq1 = self.mapa[y-1][x-1]
+                except IndexError:
                         pass
-        # for linea in self.mapa:
-        #     for nodo in linea:
-        #         x = nodo.pos_x
-        #         y = nodo.pos_y
-        #         if nodo.nodo_arriba and nodo.nodo_izq:
-        #             nodo.nodo_diag_izq1 = self.mapa[y-1][x-1]
-        #         if nodo.nodo_abajo and nodo.nodo_izq:
-        #             nodo.nodo_diag_izq2 = self.mapa[y+1][x-1]
-        #         if nodo.nodo_arriba and nodo.nodo_der:
-        #             nodo.nodo_diag_der1 = self.mapa[y-1][x+1]
-        #         if nodo.nodo_abajo and nodo.nodo_der:
-        #             nodo.nodo_diag_der2 = self.mapa[y+1][x+1]
+                try:
+                    if x > 0:
+                        nodo.nodo_diag_izq2 = self.mapa[y+1][x-1]
+                except IndexError:
+                        pass
+                try:
+                    if y > 0:
+                        nodo.nodo_diag_der1 = self.mapa[y-1][x+1]
+                except IndexError:
+                        pass
+                try:
+                     nodo.nodo_diag_der2 = self.mapa[y+1][x+1]
+                except IndexError:
+                        pass
 
 
 
@@ -81,6 +114,16 @@ class Grafo:
 a = Grafo()
 a.agregar_nodos()
 a.actualizar_nodos()
-for b in a.mapa:
-    for nodo in b:
-        #print(c.pos_x, c.pos_y, c.nodo_arriba, c.nodo_abajo, c.nodo_izq, c.nodo_der, c.nodo_diag_izq1, c.nodo_diag_izq2, c.nodo_diag_der1, c.nodo_diag_der2)
+
+for nodos in a.mapa:
+    for nodo in nodos:
+        print('......')
+        print(nodo.pos_x, nodo.pos_y)
+        print('diagder1')
+        try:
+            print(nodo.nodo_diag_der1.pos_x, nodo.nodo_diag_der1.pos_y)
+        except:
+            pass
+
+
+
